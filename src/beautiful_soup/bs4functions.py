5188791249195ba73bb74aa225ea202c2cd3c81d
LeanderShulgin <leandrohornos@gmail.com>
2022-03-14 19:54:34 -0300

    updated getAccordionInfo to handle more general content

--- src/beautiful_soup/bs4functions.py
+++ src/beautiful_soup/bs4functions.py
@@ -1,57 +1,59 @@
 from bs4 import BeautifulSoup
 import re
 
+
 def getTitle(soup):
     title = None
     main_item = soup.find("div", class_="main-item")
     title = main_item.h1.text
-    return({"title": title})
+    return {"title": title}
+
 
 # getAccordionInfo(soup: bs4_obj, split_tag: string)
 # --------------------------------------------
 
 
-# This function gets the beautifulsoup object containing the accordion code. 
-# The accordion is a component present in the course overview wich is used 
-# to display basic information about the course, splited in cards containing 
-# a title and the info. At the end of the accordion there is info that is not 
-# relevant and therefore that part of the code has to be removed. 
+# This function gets the beautifulsoup object containing the accordion code.
+# The accordion is a component present in the course overview wich is used
+# to display basic information about the course, splited in cards containing
+# a title and the info. At the end of the accordion there is info that is not
+# relevant and therefore that part of the code has to be removed.
 # The function receives a substring that indicates where to cut the code
 
-def getAccordionInfo(soup, split_tag="<h3>Testimonials</h3>"):
-    accordion = soup.find(id="accordion")
-    #config
-    changes = [{"old":"<li>", "new":"" },
-         {"old": "</li>","new": ". "},
-         {"old": "<ul>", "new": "<p>"},
-         {"old": "</ul>", "new": "</p>"},
-         {"old": "<strong>", "new": ""},
-         {"old": "</strong>", "new": " "}]
-    
-    # Accordion is splitted and only the beggining is kept
-    splitted = str(accordion).split(split_tag)[0]
-    accordion = BeautifulSoup(splitted, "lxml").find(id="accordion")
-    
-    # Get titles and divs
-    titles = accordion.find_all("h3")
-    divs = accordion.find_all("div")
-    
-    # Clean content
-    new_divs= []
 
-    for div in divs:
-        string = str(div)
-        for change in changes:
-            string = string.replace(change["old"], change["new"])
-        code = BeautifulSoup(string, "html.parser")
-        new_divs.append(code)
-        
-    # Prepare data and return it
-    data = {}
-    for i in range(len(titles)):
-        data[titles[i].text.lower().replace(" ", "-").replace("'","")] = new_divs[i].text      
-    
-    return data
+def getAccordionInfo(soup, split_tag="<h3>Testimonials</h3>"):
+    try:
+        accordion = soup.find(id="accordion")
+        # config
+        changes = [
+            {"old": "<li>", "new": ""},
+            {"old": "</li>", "new": ". "},
+            {"old": "<ul>", "new": "<p>"},
+            {"old": "</ul>", "new": "</p>"},
+            {"old": "<strong>", "new": ""},
+            {"old": "</strong>", "new": " "},
+        ]
+
+        # Accordion is splitted and only the beggining is kept
+        splitted = str(accordion).split(split_tag)[0]
+        accordion = BeautifulSoup(splitted, "lxml").find(id="accordion")
+
+        children = []
+        texts = []
+        children = accordion.findChildren(recursive=False)
+        for child in children:
+            if child.name in ["h1", "h2", "h3"]:
+                texts.append(child.text.strip().upper() + ":")
+            else:
+                texts.append(child.text.strip())
+        text = " ".join(texts)
+        # Remove any double spaces
+        text = re.sub(r"([a-z])([A-Z])", r"\1. \2", text)
+        return {"information": text}
+    except Exception as e:
+        print("Accordion information could not be scrapped:\n", e)
+        return {"information": None}
+
 
 # getDescriptionInfo(soup:bs4_obj)
 # ---------------------------------------
@@ -63,25 +65,26 @@ def getAccordionInfo(soup, split_tag="<h3>Testimonials</h3>"):
 
 def getDescriptionInfo(soup):
     description = soup.find(class_="description")
-    #config
-    tag_changes = [{"old":"<br/>", "new":"" },
-         {"old": "|","new": ""},
-         {"old": "<p><strong>", "new": "<p>"},
-         {"old": "<strong>", "new": "</p><p>"},
-         {"old": "</strong>", "new": ""},]
-     
+    # config
+    tag_changes = [
+        {"old": "<br/>", "new": ""},
+        {"old": "|", "new": ""},
+        {"old": "<p><strong>", "new": "<p>"},
+        {"old": "<strong>", "new": "</p><p>"},
+        {"old": "</strong>", "new": ""},
+    ]
+
     # Get the raw data from the description html
 
     raw_pitch = description.find_all("p")
 
-    
     # Unify the pitch in one single text
     pitch = ""
     for p in raw_pitch:
         pitch = pitch + " " + p.text
     pitch = pitch.strip()
-    
-    return({"pitch": pitch})
+
+    return {"pitch": pitch}
 
 
 # getFeeInfo(soup:bs4_obj):
@@ -90,24 +93,25 @@ def getDescriptionInfo(soup):
 # This function takes a code soup and identifies the
 # div that contains the fee info, and then extracts it
 
+
 def getFeeInfo(soup):
     no_result = {"price": None, "currency": None}
     boxes = soup.find_all("div", class_="box")
     box = None
     for div in boxes:
-        if(div.h3):
+        if div.h3:
             if div.h3.text.upper() == "FEE INFO":
                 box = div
     if not box:
         return no_result
     price = box.find("span", class_="price")
     if not price:
-         return no_result
+        return no_result
     value = None
     match = re.match("^\£?\$?\d+\.\d+", price.text)
     if match:
         value = match.group(0)
-    return {"price": value[1:], "currency": value[0] }
+    return {"price": value[1:], "currency": value[0]}
 
 
 # scrapCourse(soup:bs4_obj):
@@ -117,8 +121,9 @@ def getFeeInfo(soup):
 # in order to completely scrap all the info
 # in a course overview page, feeded as a bs4 object
 
+
 def scrapCourse(soup):
-    #Scrap all data
+    # Scrap all data
     title = getTitle(soup)
     information = getAccordionInfo(soup)
     description = getDescriptionInfo(soup)
